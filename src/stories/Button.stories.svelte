4d28d3d3f9a67ccaeb7066831d3177707f019b80
Jean-Baptiste Berthet <jberthet@thefork.com>
2025-02-19 19:16:49 +0100

    feat(dark-mode): add dark mode

--- src/stories/Button.stories.svelte
+++ src/stories/Button.stories.svelte
@@ -1,24 +1,34 @@
 <script module>
-  import { defineMeta } from '@storybook/addon-svelte-csf';
-  import Button from './Button.svelte';
-  import { fn } from '@storybook/test';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+	import { fn } from '@storybook/test';
+	import { allModes } from '../../.storybook/modes.js';
 
-  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-  const { Story } = defineMeta({
-    title: 'Example/Button',
-    component: Button,
-    tags: ['autodocs'],
-    argTypes: {
-      backgroundColor: { control: 'color' },
-      size: {
-        control: { type: 'select' },
-        options: ['small', 'medium', 'large'],
-      },
-    },
-    args: {
-      onClick: fn(),
-    }
-  });
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Example/Button',
+		component: Button,
+		tags: ['autodocs'],
+		argTypes: {
+			backgroundColor: { control: 'color' },
+			size: {
+				control: { type: 'select' },
+				options: ['small', 'medium', 'large']
+			}
+		},
+		args: {
+			onClick: fn()
+		},
+		parameters: {
+			layout: 'centered',
+			chromatic: {
+				modes: {
+					light: allModes['light'],
+					dark: allModes['dark']
+				}
+			}
+		}
+	});
 </script>
 
 <!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
